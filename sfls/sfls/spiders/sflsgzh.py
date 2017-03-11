398a3b34343da61d0b737c7d575cfa122db82e3c
root <root@ip-172-31-22-0.ap-southeast-1.compute.internal>
2017-03-11 08:11:51 +0000

    initial

--- sfls/sfls/spiders/sflsgzh.py
+++ sfls/sfls/spiders/sflsgzh.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import scrapy
 import re
 def striphtml(data):
@@ -17,12 +18,23 @@ class SFLSGZHSpider(scrapy.Spider):
             img = li.xpath('div/div[1]/a/img/@src')
             name = striphtml(li.xpath('div/div[2]/p[1]/a').extract_first())
             info = li.xpath('div/div[2]/p[2]/label/text()').extract_first()
+            data = li.xpath('div/div[2]/p[2]/text()').extract()
+            try:
+                data = re.split(u'月发文|篇|平均阅读',data[2])
+                post_month = int(data[1])
+                read_avg = int(data[3])
+            except IndexError:
+                post_month = 0
+                read_avg = 0
+           
             qrcode = li.xpath('div/div[3]/span/img[1]/@src')
             jieshao = striphtml(li.xpath('dl[1]/dd').extract_first())
             renzhen = li.xpath('dl[2]/dd/text()').extract_first()
             yield {
                 'name': name,
                 'info': info,
+                'post_month': post_month,
+                'read_avg': read_avg,
                 'jieshao': jieshao    
             }
 '''
