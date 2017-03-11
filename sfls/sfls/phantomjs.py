4e5c3973d3b5d5c58d56494b5673f7f60a7ff045
root <root@ip-172-31-22-0.ap-southeast-1.compute.internal>
2017-03-11 08:15:22 +0000

    initial

---
+++ sfls/sfls/phantomjs.py
@@ -0,0 +1,10 @@
+from scrapy.http import HtmlResponse
+from selenium import webdriver
+
+class JSMiddleware(object):
+    def process_request(self, request, spider):
+        driver = webdriver.PhantomJS()
+        driver.get(request.url)
+
+        body = driver.page_source
+        return HtmlResponse(driver.current_url, body=body, encoding='utf-8', request=request)
